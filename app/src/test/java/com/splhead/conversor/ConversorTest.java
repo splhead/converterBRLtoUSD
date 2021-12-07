a8a44513ec89757784164cd27a2445dde821da95
splhead <splhead@gmail.com>
2021-12-06 22:26:44 -0400

    create DriverFactory

--- app/src/test/java/com/splhead/conversor/ConversorTest.java
+++ app/src/test/java/com/splhead/conversor/ConversorTest.java
@@ -1,40 +1,26 @@
 package com.splhead.conversor;
 
-import io.appium.java_client.MobileElement;
-import io.appium.java_client.android.AndroidDriver;
-import junit.framework.TestCase;
+import com.splhead.conversor.util.DriverFactory;
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import java.net.MalformedURLException;
-import java.net.URL;
-import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
 
 public class ConversorTest {
 
-    private AndroidDriver driver;
+    private AndroidDriver<MobileElement> driver;
 
     @Before
-    public void setUp() throws MalformedURLException {
-        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-        desiredCapabilities.setCapability("platformName", "Android");
-        desiredCapabilities.setCapability("appium:deviceName", "0027461287");
-        desiredCapabilities.setCapability("appium:automationName", "uiautomator2");
-        desiredCapabilities.setCapability("appium:appPackage", "com.splhead.conversor");
-        desiredCapabilities.setCapability("appium:appActivity", "com.splhead.conversor.MainActivity");
-        desiredCapabilities.setCapability("appium:ensureWebviewsHavePages", true);
-        desiredCapabilities.setCapability("appium:nativeWebScreenshot", true);
-        desiredCapabilities.setCapability("appium:newCommandTimeout", 0);
-        desiredCapabilities.setCapability("appium:connectHardwareKeyboard", true);
-
-        URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
-
-        driver = new AndroidDriver(remoteUrl, desiredCapabilities);
+    public void setUp() {
+        driver = DriverFactory.getDriver();
     }
 
     @Test
-    public void testConvertionTwoReaisToDollars() {
+    public void testConversionTwoBRLToDollars() {
         MobileElement etReal = (MobileElement) driver.findElementById("com.splhead.conversor:id/etReal");
         etReal.sendKeys("2");
         MobileElement bConvert = (MobileElement) driver.findElementById("com.splhead.conversor:id/bConvert");
@@ -45,6 +31,6 @@ public class ConversorTest {
 
     @After
     public void tearDown() {
-        driver.quit();
+        DriverFactory.destroyDriver();
     }
 }
